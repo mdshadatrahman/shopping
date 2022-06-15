90d7152435ae1fd2dfa4c723940e9debf068b4ab
Shadat Rahman <shadat.rahman.464@gmail.com>
2022-06-15 13:07:59 +0600

    added folders and color manager

---
+++ lib/utils/color_manager.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static Color background = const Color(0xffF7F2FF);
+  static Color white = const Color(0xffFFFFFF);
+  static Color circleButtonColorBlue1 = const Color(0xff6210E1);
+  static Color circleButtonColorBlue2 = const Color(0xff1400AE);
+  static Color circleButtonColorPink1 = const Color(0xffFFBFDD);
+  static Color purpleText = const Color(0xffDA2079);
+  static Color nameTextColor = const Color(0xff323232);
+  static Color priceTextColor = const Color(0xff646464);
+  static Color addButtonBackground = const Color(0xffFFCCE4);
+}
